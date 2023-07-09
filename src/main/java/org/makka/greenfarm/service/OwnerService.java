f4ded517edca0910a88905513981fadf4638267f
lenovo <629848089@qq.com>
2023-07-09 11:57:50 +0800

    zsx 第十三次提交到本地仓库（农场主登录注册）

--- src/main/java/org/makka/greenfarm/service/OwnerService.java
+++ src/main/java/org/makka/greenfarm/service/OwnerService.java
@@ -1,7 +1,29 @@
 package org.makka.greenfarm.service;
 
 import com.baomidou.mybatisplus.extension.service.IService;
+import org.makka.greenfarm.common.CommonResponse;
 import org.makka.greenfarm.domain.Owner;
+import org.makka.greenfarm.domain.User;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.http.HttpServletRequest;
 
 public interface OwnerService extends IService<Owner> {
+
+    public Owner getOwnerByOwnername(String username);
+
+    //验证用户名和密码是否匹配
+    public boolean validation(String name, String password);
+    //注册
+    public CommonResponse<String> register(Owner owner);
+
+    public String getOidByOwnername(String username);
+
+    public boolean updateAvatar(String oid, HttpServletRequest request, MultipartFile file);
+
+    public boolean updateOwnerInfo(Owner owner);
+
+    public Owner getOwnerInfo(String oid);
+
+
 }
