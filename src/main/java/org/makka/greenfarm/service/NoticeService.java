72e9df8e417f8f21e43a543eddb240060b5b002b
lin <149472644@qq.com>
2023-07-09 11:53:39 +0800

    公告列表和公告详情查看

--- src/main/java/org/makka/greenfarm/service/NoticeService.java
+++ src/main/java/org/makka/greenfarm/service/NoticeService.java
@@ -3,5 +3,10 @@ package org.makka.greenfarm.service;
 import com.baomidou.mybatisplus.extension.service.IService;
 import org.makka.greenfarm.domain.Notice;
 
+import java.util.List;
+
 public interface NoticeService extends IService<Notice> {
+    public List<Notice> getNoticeList();
+
+    public Notice getNoticeByNid(String nid);
 }
