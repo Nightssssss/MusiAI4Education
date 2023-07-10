01e268d3882bbac3a9afc0d4da5ec98d1087cf9f
lin <149472644@qq.com>
2023-07-11 01:00:54 +0800

    默认虚拟形象

--- src/main/java/org/makka/greenfarm/service/impl/UserServiceImpl.java
+++ src/main/java/org/makka/greenfarm/service/impl/UserServiceImpl.java
@@ -61,6 +61,7 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements Us
             }else{
                 user.setAvatar("http://localhost:8080/images/user/avatar/default.png");
             }
+            user.setVirtualization("http://175.178.5.157:8080/images/user/virtualization/default.gif");
             // 不存在则插入
             userMapper.insert(user);
             return CommonResponse.creatForSuccess("注册成功");
@@ -95,6 +96,7 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements Us
         user.setUsername(user1.getUsername());
         user.setPassword(user1.getAvatar());
         user.setAvatar(user1.getAvatar());
+        user.setVirtualization(user1.getVirtualization());
         int result = userMapper.updateById(user);
         if (result == 1) {
             return true;
