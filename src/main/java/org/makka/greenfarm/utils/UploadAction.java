612c6797f68c972f49b82e972c8e0e7a5e16a2a2
lin <149472644@qq.com>
2023-07-11 21:09:41 +0800

    发布公告图片

--- src/main/java/org/makka/greenfarm/utils/UploadAction.java
+++ src/main/java/org/makka/greenfarm/utils/UploadAction.java
@@ -62,4 +62,28 @@ public class UploadAction {
             return null;
         }
     }
+
+    public static String uploadNoticeImage(HttpServletRequest request, MultipartFile file) {
+        File dir = new File(fileSavePath);
+        if (!dir.exists()) {
+            dir.mkdirs();
+        }
+        System.out.println("图片上传，保存位置：" + fileSavePath);
+        //3.给文件重新设置一个名字
+        //后缀
+        String suffix = file.getOriginalFilename().substring(file.getOriginalFilename().lastIndexOf("."));
+        String newFileName= UUID.randomUUID().toString().replaceAll("-", "")+suffix;
+        //4.创建这个新文件
+        File newFile = new File(fileSavePath + "/notice/" + newFileName);
+        //5.复制操作
+        try {
+            file.transferTo(newFile);
+            //协议 :// ip地址 ：端口号 / 文件目录(/images/2020/03/15/xxx.jpg)
+            String url = request.getScheme() + "://" + request.getServerName() + ":" + request.getServerPort() + "/images/notice/"  + newFileName;
+            System.out.println("图片上传，访问URL：" + url);
+            return url;
+        } catch (IOException e) {
+            return null;
+        }
+    }
 }
